7c4c5632a317e0ef4cd16f862c8d172a0d223d68
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-27 14:23:28 +0100

    ADD: Correzioni

--- snack.js
+++ snack.js
@@ -7,12 +7,18 @@ async function fetchJson(url) {
 // const getChefBirthday = async (id) => {
 //     const recipe = await fetchJson(`https://dummyjson.com/recipes/${id}`)
 //     const user = await fetchJson(`https://dummyjson.com/users/${recipe.userId}`)
-//     return user.age
+//     return user.birthDate
 // }
 
 // (async () => {
-//     const chefBirthday = await getChefBirthday(2)
-//     console.log(chefBirthday)
+//     try {
+//         const chefBirthday = await getChefBirthday(2)
+//         console.log("Compleanno dello chef:", chefBirthday)
+//     } catch (error) {
+//         console.error(error)
+//     } finally {
+//         console.log("ciao")
+//     }
 // })()
 
 
@@ -22,23 +28,29 @@ async function getChefBirthday(id) {
     try {
         recipe = await fetchJson(`https://dummyjson.com/recipes/${id}`)
     } catch (error) {
+        console.error(error)
         throw new Error(`Non posso recuperare la ricetta con id: ${id}`)
     }
 
-    if (!recipe) {
-        throw new Error(`Ricetta con id: ${id} non valida`)
+    if (recipe.message) {
+        throw new Error(recipe.message)
     }
 
     let user
     try {
         user = await fetchJson(`https://dummyjson.com/users/${recipe.userId}`)
     } catch (error) {
+        console.error(error)
         throw new Error(`Non posso recuperare lo chef con id: ${id}`)
+    }
+
+    if (user.message) {
+        throw new Error(user.message)
     }
 
     return user.birthDate
 }
 
-getChefBirthday(1335)
+getChefBirthday(1)
     .then(birthday => console.log("Data di nascita dello chef:", birthday))
     .catch(error => console.error("Errore:", error.message));
