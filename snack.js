d0b9199ce3d5ae44e6d5cd1eded25983054ae54b
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-27 14:31:37 +0100

    ADD: dayjs library and formatted birth date

--- snack.js
+++ snack.js
@@ -7,7 +7,9 @@ async function fetchJson(url) {
 // const getChefBirthday = async (id) => {
 //     const recipe = await fetchJson(`https://dummyjson.com/recipes/${id}`)
 //     const user = await fetchJson(`https://dummyjson.com/users/${recipe.userId}`)
-//     return user.birthDate
+
+//     const formattedBirthDate = dayjs(user.birthDate).format("DD/MM/YYYY")
+//     return formattedBirthDate
 // }
 
 // (async () => {
@@ -48,9 +50,12 @@ async function getChefBirthday(id) {
         throw new Error(user.message)
     }
 
-    return user.birthDate
+    const formattedBirthDate = dayjs(user.birthDate).format("DD/MM/YYYY")
+    return formattedBirthDate
 }
 
 getChefBirthday(1)
     .then(birthday => console.log("Data di nascita dello chef:", birthday))
     .catch(error => console.error("Errore:", error.message));
+
+console.log(dayjs)
